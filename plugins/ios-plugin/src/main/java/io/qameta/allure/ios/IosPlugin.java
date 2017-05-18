7cacc7cc7681bb07378b9aaec227bd64cb0a92b0
Artem Eroshenko <eroshenkoam@qameta.io>
2017-05-18 17:43:47 +0300

    add attachment and time

--- plugins/ios-plugin/src/main/java/io/qameta/allure/ios/IosPlugin.java
+++ plugins/ios-plugin/src/main/java/io/qameta/allure/ios/IosPlugin.java
@@ -3,8 +3,8 @@ package io.qameta.allure.ios;
 import io.qameta.allure.Reader;
 import io.qameta.allure.core.Configuration;
 import io.qameta.allure.core.ResultsVisitor;
-import io.qameta.allure.entity.StageResult;
 import io.qameta.allure.entity.Status;
+import io.qameta.allure.entity.StatusDetails;
 import io.qameta.allure.entity.Step;
 import io.qameta.allure.entity.TestResult;
 import org.slf4j.Logger;
@@ -16,12 +16,17 @@ import java.io.IOException;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.DateTimeException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.Objects;
-import java.util.UUID;
-import java.util.stream.Collectors;
+import java.util.Optional;
 
 import static java.nio.file.Files.newDirectoryStream;
 import static java.util.Collections.emptyList;
@@ -36,12 +41,11 @@ public class IosPlugin implements Reader {
     private static final String TESTABLE_SUMMARIES = "TestableSummaries";
     private static final String TESTS = "Tests";
     private static final String SUB_TESTS = "Subtests";
-    private static final String TEST_STATUS = "TestStatus";
-    private static final String TEST_NAME = "TestName";
-    private static final String TEST_IDENTIFIER = "TestIdentifier";
     private static final String TITLE = "Title";
     private static final String SUB_ACTIVITIES = "SubActivities";
     private static final String ACTIVITY_SUMMARIES = "ActivitySummaries";
+    private static final String HAS_SCREENSHOT = "HasScreenshotData";
+
 
     @Override
     public void readResults(final Configuration configuration,
@@ -53,82 +57,81 @@ public class IosPlugin implements Reader {
                 LOGGER.info("Parse file {}", file);
                 final Map<String, Object> loaded = Plist.load(file.toFile());
                 final List<?> summaries = asList(loaded.getOrDefault(TESTABLE_SUMMARIES, emptyList()));
-                summaries.forEach(summary -> parseSummary(summary, visitor));
+                summaries.forEach(summary -> parseTestRun(directory, summary, visitor));
             } catch (XmlParseException | IOException e) {
                 LOGGER.error("Could not parse file {}: {}", file, e);
             }
         }
     }
 
-    private void parseSummary(final Object summary, final ResultsVisitor visitor) {
+    private void parseTestRun(final Path directory, final Object summary, final ResultsVisitor visitor) {
         final Map<String, Object> props = asMap(summary);
         final List<Object> tests = asList(props.getOrDefault(TESTS, emptyList()));
-        tests.forEach(test -> parseTestSuite(test, visitor));
+        tests.forEach(test -> parseTestSuite(directory, test, visitor));
     }
 
     @SuppressWarnings("unchecked")
-    private void parseTestSuite(final Object testSuite, final ResultsVisitor visitor) {
+    private void parseTestSuite(final Path directory, final Object testSuite, final ResultsVisitor visitor) {
         Map<String, Object> props = asMap(testSuite);
-        if (isTest(props)) {
-            parseTest(testSuite, visitor);
+        if (ResultsUtils.isTest(props)) {
+            parseTest(directory, testSuite, visitor);
             return;
         }
 
         final Object tests = props.get(SUB_TESTS);
         if (Objects.nonNull(tests)) {
             final List<?> subTests = List.class.cast(tests);
-            subTests.forEach(subTest -> parseTestSuite(subTest, visitor));
+            subTests.forEach(subTest -> parseTestSuite(directory, subTest, visitor));
         }
     }
 
-    private void parseTest(final Object test, final ResultsVisitor visitor) {
+    private void parseTest(final Path directory, final Object test, final ResultsVisitor visitor) {
         Map<String, Object> props = asMap(test);
-        final List<Step> steps = asList(props.getOrDefault(ACTIVITY_SUMMARIES, emptyList())).stream()
-                .map(this::parseStep)
-                .collect(Collectors.toList());
-        final TestResult result = new TestResult()
-                .withName(getTestName(props))
-                .withStatus(getTestStatus(props))
-                .withFullName(getFullName(props))
-                .withUid(UUID.randomUUID().toString());
-        if (!steps.isEmpty()) {
-            result.setTestStage(new StageResult().withSteps(steps));
-        }
+        final TestResult result = ResultsUtils.getTestResult(props);
+        asList(props.getOrDefault(ACTIVITY_SUMMARIES, emptyList()))
+                .forEach(activity -> parseStep(directory, result, result, activity, visitor));
         visitor.visitTestResult(result);
     }
 
-    private Step parseStep(final Object step) {
-        final Map<String, Object> props = asMap(step);
-        final String stepName = (String) props.get(TITLE);
-        final List<Step> subSteps = asList(props.getOrDefault(SUB_ACTIVITIES, emptyList())).stream()
-                .map(this::parseStep)
-                .collect(Collectors.toList());
-        return new Step()
-                .withName(stepName)
-                .withStatus(Status.PASSED)
-                .withSteps(subSteps);
-    }
+    private void parseStep(final Path directory, final TestResult testResult, final Object parent,
+                           final Object activity, final ResultsVisitor visitor) {
+        final Map<String, Object> props = asMap(activity);
+        final String activityTitle = (String) props.get(TITLE);
 
-    private String getTestName(final Map<String, Object> props) {
-        return (String) props.getOrDefault(TEST_NAME, "Unknown");
-    }
+        if (activityTitle.startsWith("Assertion Failure:")) {
+            testResult.setStatusDetails(new StatusDetails().withMessage(activityTitle));
+            ((Step) parent).setStatus(Status.FAILED);
+            ((Step) parent).setStatusDetails(new StatusDetails().withMessage(activityTitle));
+            return;
+        }
 
-    private String getFullName(final Map<String, Object> props) {
-        return (String) props.get(TEST_IDENTIFIER);
-    }
+        if (activityTitle.startsWith("Start Test at")) {
+            getStartTime(activityTitle).ifPresent(start -> {
+                long duration = testResult.getTime().getDuration();
+                testResult.getTime().setStart(start);
+                testResult.getTime().setStop(start + duration);
+            });
+            return;
+        }
 
-    private Status getTestStatus(final Map<String, Object> props) {
-        final Object status = props.get(TEST_STATUS);
-        if (Objects.isNull(status)) {
-            return Status.UNKNOWN;
+        final Step step = ResultsUtils.getStep(props);
+
+        if (props.containsKey(HAS_SCREENSHOT)) {
+            String uuid = props.get("UUID").toString();
+            Path attachmentPath = directory.resolve("Attachments").resolve(String.format("Screenshot_%s.png", uuid));
+            step.getAttachments().add(visitor.visitAttachmentFile(attachmentPath));
         }
-        if ("Success".equals(status)) {
-            return Status.PASSED;
+
+        if (parent instanceof TestResult) {
+            ((TestResult) parent).getTestStage().getSteps().add(step);
         }
-        if ("Failure".equals(status)) {
-            return Status.FAILED;
+
+        if (parent instanceof Step) {
+            ((Step) parent).getSteps().add(step);
         }
-        return Status.UNKNOWN;
+
+        asList(props.getOrDefault(SUB_ACTIVITIES, emptyList()))
+                .forEach(subActivity -> parseStep(directory, testResult, step, subActivity, visitor));
     }
 
     @SuppressWarnings("unchecked")
@@ -139,10 +142,6 @@ public class IosPlugin implements Reader {
     @SuppressWarnings("unchecked")
     private Map<String, Object> asMap(final Object object) {
         return Map.class.cast(object);
-    }
-
-    private boolean isTest(final Map<String, Object> props) {
-        return props.containsKey(TEST_STATUS);
     }
 
     private static List<Path> listResults(final Path directory) {
@@ -162,4 +161,15 @@ public class IosPlugin implements Reader {
         }
         return result;
     }
+
+    private static Optional<Long> getStartTime(final String stepName) {
+        try {
+            final DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSSX", Locale.US);
+            final Date date = dateFormat.parse(stepName.substring(14));
+            return Optional.of(date.getTime());
+        } catch (DateTimeException | ParseException e) {
+            return Optional.empty();
+        }
+    }
+
 }
---
+++ plugins/ios-plugin/src/main/java/io/qameta/allure/ios/ResultsUtils.java
@@ -0,0 +1,93 @@
+package io.qameta.allure.ios;
+
+import io.qameta.allure.entity.StageResult;
+import io.qameta.allure.entity.Status;
+import io.qameta.allure.entity.Step;
+import io.qameta.allure.entity.TestResult;
+import io.qameta.allure.entity.Time;
+
+import java.util.Map;
+import java.util.Objects;
+import java.util.UUID;
+
+/**
+ * The collection of Test result utils methods.
+ */
+public final class ResultsUtils {
+
+    private static final String TEST_NAME = "TestName";
+    private static final String TEST_STATUS = "TestStatus";
+    private static final String TEST_DURATION = "Duration";
+    private static final String TEST_IDENTIFIER = "TestIdentifier";
+
+    private static final String STEP_NAME = "Title";
+    private static final String STEP_START_TIME = "StartTimeInterval";
+    private static final String STEP_STOP_TIME = "FinishTimeInterval";
+
+    private ResultsUtils() {
+    }
+
+    public static TestResult getTestResult(final Map<String, Object> props) {
+        return new TestResult()
+                .withUid(UUID.randomUUID().toString())
+                .withName(ResultsUtils.getTestName(props))
+                .withStatus(ResultsUtils.getTestStatus(props))
+                .withFullName(ResultsUtils.getFullName(props))
+                .withTime(ResultsUtils.getTestTime(props))
+                .withTestStage(new StageResult());
+    }
+
+    public static boolean isTest(final Map<String, Object> props) {
+        return props.containsKey(TEST_STATUS);
+    }
+
+    private static String getTestName(final Map<String, Object> props) {
+        return (String) props.getOrDefault(TEST_NAME, "Unknown");
+    }
+
+    private static String getFullName(final Map<String, Object> props) {
+        return (String) props.getOrDefault(TEST_IDENTIFIER, "Unknown");
+    }
+
+    private static Time getTestTime(final Map<String, Object> props) {
+        return new Time().withDuration(parseTime(props.getOrDefault(TEST_DURATION, "0").toString()));
+    }
+
+    private static Status getTestStatus(final Map<String, Object> props) {
+        final Object status = props.get(TEST_STATUS);
+        if (Objects.isNull(status)) {
+            return Status.UNKNOWN;
+        }
+        if ("Success".equals(status)) {
+            return Status.PASSED;
+        }
+        if ("Failure".equals(status)) {
+            return Status.FAILED;
+        }
+        return Status.UNKNOWN;
+    }
+
+
+    public static Step getStep(final Map<String, Object> props) {
+        return new Step()
+                .withName(getStepName(props))
+                .withTime(getStepTime(props))
+                .withStatus(Status.PASSED);
+    }
+
+    private static String getStepName(final Map<String, Object> props) {
+        return (String) props.getOrDefault(STEP_NAME, "Unknown");
+    }
+
+    private static Time getStepTime(final Map<String, Object> props) {
+        long start = parseTime(props.getOrDefault(STEP_START_TIME, "0").toString());
+        long stop = parseTime(props.getOrDefault(STEP_STOP_TIME, "0").toString());
+        return new Time().withStart(start).withStop(stop).withDuration(stop - start);
+    }
+
+    private static long parseTime(final String time) {
+        final Double doubleTime = Double.parseDouble(time);
+        final int seconds = doubleTime.intValue();
+        return seconds * 1000;
+    }
+}
