07e06cdfb2978d5495ef7c16315f149038363f8d
Alena Liashenka <eichhornchen.27@gmail.com>
2017-11-23 11:44:06 +0300

    Use TreeViewContainer for MappingsTreeLayout to fix 404 error

--- allure-generator/src/main/javascript/plugins/tab-mapping/MappingsTreeView.js
+++ allure-generator/src/main/javascript/plugins/tab-mapping/MappingsTreeView.js
@@ -1,14 +1,16 @@
 import '../../components/tree/styles.scss';
 import {behavior, className, regions} from '../../decorators';
-import TreeView from '../../components/tree/TreeView';
+import TreeViewContainer from '../../components/tree-view-container/TreeViewContainer';
 
 @className('tree')
 @behavior('TooltipBehavior', {position: 'bottom'})
 @regions({
+    search: '.pane__search',
     sorter: '.tree__sorter',
-    filter: '.tree__filter'
+    filter: '.tree__filter',
+    content: '.tree__content'
 })
-class MappingsTreeView extends TreeView {
+class MappingsTreeView extends TreeViewContainer {
 
     findTestResult(treeNode) {
         return this.$(`[data-uid='${treeNode.testResult}']`);
